5f52174c7034c495d566d17f51ecb01138181d32
AndreiLidzhiev <480211@student.saxion.nl>
2022-05-11 12:24:21 +0200

    code: refinement of map code

--- src/frontend/pages/gmaps/gmap.tsx
+++ src/frontend/pages/gmaps/gmap.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
-export default function GMap() {
+
+interface MapProps extends google.maps.MapOptions {
+    style : { [key: string]: string };
+    onClick?: (e: google.maps.MapMouseEvent) => void;
+    onIdle?: (map: google.maps.Map) => void;
+  }
+  
+
+
+const Map: React.FC<MapProps> = ({onClick}) => {
 
     //const Map: React.FC<{}> = () => {};
 
@@ -12,6 +21,8 @@ export default function GMap() {
         setMap(new window.google.maps.Map(ref.current, {}));
     }
     }, [ref, map]);
-
+    
     return <div ref={ref} />
 }
+
+export default Map;
