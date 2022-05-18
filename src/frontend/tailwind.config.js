864003edb99a16b4ea10dee130e41cfe58e0d56a
Daria <gri33dar@gmail.com>
2022-05-18 10:59:32 +0200

    feat(frontend): created navigation component to test react.js, tailwinds css

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,html}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ], theme: {
     extend: {},
   },
   plugins: [],
 }
+
