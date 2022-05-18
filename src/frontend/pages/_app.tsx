db2aaecfade2420c519f4e308dd7cdd0316fe6e0
vedatdd <90068068+vedatdd@users.noreply.github.com>
2022-05-18 12:26:20 +0200

    template for working google map

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import { AppProps } from 'next/app'
+import { Wrapper } from "@googlemaps/react-wrapper";
+import { AppProps } from "next/app";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+export default function MyApp({ Component, pageProps}: AppProps) {
+    return(
+        <Wrapper
+            apiKey={"AIzaSyAu53ClDbYWFVWX60wEfEQ5Ed4R-9lYU8E"}
+            libraries={['places', 'geometry']}
+            language={'ro'}
+        >
+            <Component {...pageProps}/>
+        </Wrapper>
+    );
+};
