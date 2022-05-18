73d198684a5fc98376cf17449fd18b39d69a6d0c
Daria <gri33dar@gmail.com>
2022-05-19 00:07:26 +0200

    Frontend(navigation.tsx): responsiveness

--- src/frontend/pages/navigation.tsx
+++ src/frontend/pages/navigation.tsx
@@ -7,14 +7,13 @@ import styles from '../styles/Home.module.css'
 const Navigation: NextPage = () => {
 
     return (
-        <div  className="h-full ">
-            <div id="header" className="p-4 bg-gray-400 ">Header</div>
+        <div  className="h-screen border-black border-4 bg-gray-900">
+            <div id="header" className="h-1/6 bg-gray-400 border-blue-500 border-2 mx-4 rounded-lg">Header</div>
 
-            <div id="main" className="h-1/2 max-h-full p-4 sm:h-full  bg-gray-200 ">
+            <div id="main" className="h-5/6   bg-gray-200 border-red-900 border-2">
 
-                Content
-                <div>                Content
-                </div>
+                MAP
+
 
             </div>
 
