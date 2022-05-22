f06fb2b128a2ec0490abe2d627bffa2086705c08
Minh Pham <491537@student.saxion.nl>
2022-05-23 00:46:51 +0200

    chore: fix styling

--- src/frontend/pages/landing.tsx
+++ src/frontend/pages/landing.tsx
@@ -5,11 +5,15 @@ const LandingPage: NextPage = () => {
     return (
         <div className="block h-screen bg-cover bg-landing-page">
             <div className="flex flex-col items-center justify-center h-screen bg-gray-600 opacity-70">
-                <h1 className="text-2xl text-center text-white">FYWI <br></br> Walstraat</h1>
-                <div className='flex flex-col items-center justify-center mt-12 '>    
-                    <button className="px-3 py-2 font-bold text-center text-indigo-400 bg-gray-900 border-2 border-indigo-400 rounded scroll-py-20 focus:outline-none">Button</button>
-                    <button className="px-3 py-2 font-bold text-center text-indigo-400 bg-gray-900 border-2 border-indigo-400 rounded focus:outline-none">Button</button>
-                    <p>Continue without log in </p>
+                <h1 className="text-5xl text-center text-white">FYWI <br></br> Walstraat</h1>
+                <div className='flex flex-col items-center mt-12 justify-evenly h-1/5'>    
+                    <button className="px-4 py-2 font-bold text-white bg-green-700 rounded hover:bg-green-800">
+                        Sign in
+                    </button>      
+                    <button className="px-4 py-2 font-bold text-green-700 bg-white border-2 border-green-700 rounded hover:bg-gray-400">
+                        Sign up
+                    </button> 
+                    <a href='' className="hover:text-gray-900">Continue without log in </a>
                 </div>
             </div>
         </div>
