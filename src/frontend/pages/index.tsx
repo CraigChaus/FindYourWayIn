e0220786e7617ee633dde1a8bfe74058da6bd7e0
Craig Chauraya <500215@student.saxion.nl>
2022-05-23 18:24:37 +0200

    feat: Implement focus on user device location

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import GoogleAutocomplete from "../components/GoogleMaps/GoogleAutocomplete";
 import GoogleMap from "../components/GoogleMaps/GoogleMap";
 import GoogleMarker from "../components/GoogleMaps/GoogleMarker";
+import DeviceLocation from "../components/GoogleMaps/GoogleGeolocator";
 
 const HomePage = () => {
     const [ mounted, setMounted ] = React.useState(false);
@@ -18,20 +19,22 @@ const HomePage = () => {
     const [ neighborhood, setNeighborhood ] = React.useState< string >();
     const [ address, setAddress ] = React.useState< string >('');
 
-    React.useEffect(() => {
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(
-                (pos) => {
-                    const position = pos.coords;
-                    if(position){
-                        setLat(position.latitude);
-                        setLng(position.longitude);
+    const [isLocation, setIsLocation] = React.useState(false);
+
+        React.useEffect(() => {
+            if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition(
+                    (pos) => {
+                        const position = pos.coords;
+                        if(position){
+                            setLat(position.latitude);
+                            setLng(position.longitude);
+                        }
                     }
-                }
-            );
-        };
-        setMounted(true);
-    }, []);
+                );
+            };
+            setMounted(true);
+        }, [isLocation]);
 
     React.useEffect(() => {
         if(!mounted) return;
@@ -95,6 +98,12 @@ const HomePage = () => {
                     setAddress={setAddress}
                 />
             </GoogleMap>
+
+            {/* TODO: Ask for help on this part cause it only works once */}
+
+            {/* This is for the button that enables the user to focus the map back to their current position */}
+            <button onClick={() => {setIsLocation(true)} } className="absolute text-blue-800 z-10 bottom-20 right-5 p-0.5 border-2 border-blue-800 font-bold" >Get Device Location</button>
+
             <section style={{
                 margin: '20px 0 20px 0'
             }}>
