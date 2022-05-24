2f2f383aa52f6bbc77ec983d9d89de9cac6d7397
486702D <75730085+486702D@users.noreply.github.com>
2022-05-24 15:30:43 +0200

    frontend(Components/navigation, pages/navigation): changed Categories component using 'props'

--- src/frontend/components/navigation/Categories.tsx
+++ src/frontend/components/navigation/Categories.tsx
@@ -1,74 +1,28 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBicycle, faBuildingColumns, faSquareParking, faToilet, faUtensils, faCartShopping} from "@fortawesome/free-solid-svg-icons";
 
-export const Categories = () => {
+export const Categories = (props) => {
+    const categories = props.categories;
+    const iconeName = props.iconeName;
+    const nameOfCategory = props.nameOfCategory;
+    
+
     return (
         <>
-            <div  className="  flex flex-col  ">
+            {categories.map((categorie)=>(
+                <div  className="  flex flex-col  ">
 
-                <div className=" flex justify-center h-3/5 ">
-                    <button  className="flex justify-center h-12 w-12 mx-2   hover:bg-zinc-300 rounded ">
-                        <FontAwesomeIcon  icon={faCartShopping} size="2x"  className=" text-green-800 flex justify-center mt-2 "/></button>
-                </div>
-                <div className=" flex justify-center  h-2/5  " >
-                    <p className=" font-bold text-sm ">Shops</p>
-                </div>
-            </div>
-            <div className=" ">
+                    <div className=" flex justify-center h-3/5 ">
+                        <button  className="flex justify-center h-12 w-12 mx-2   hover:bg-zinc-300 rounded ">
+                            <FontAwesomeIcon  icon={categorie.iconeName} size="2x"  className=" text-green-800 flex justify-center mt-2 "/>
+                        </button>
+                    </div>
+                    <div className=" flex justify-center  h-2/5  " >
+                        <p className=" font-bold text-sm ">{categorie.nameOfCategory}</p>
+                    </div>
+                </div>  
+            ))}
 
-                <div className=" flex justify-center h-3/5  ">
-                    <button  className="flex justify-center h-12 w-12 mx-2  hover:bg-zinc-300 rounded  ">
-                        <FontAwesomeIcon  icon={faSquareParking} size="2x"  className=" text-green-800 flex justify-center mt-2 "/>
-                    </button>
-                </div>
-                <div className=" flex justify-center h-2/5   " >
-                    <p className=" font-bold text-sm ">Car parking</p>
-                </div>
-            </div>
-            <div className=" ">
-
-                <div className=" flex justify-center h-3/5 ">
-                    <button  className="flex justify-center h-12 w-12 mx-2  hover:bg-zinc-300 rounded ">
-                        <FontAwesomeIcon  icon={faToilet} size="2x"  className="text-green-800 flex justify-center mt-2"/>
-                    </button>
-                </div>
-                <div className=" flex justify-center h-2/5   " >
-                    <p className=" font-bold text-sm ">Toilet</p>
-                </div>
-            </div>
-            <div className="">
-
-                <div className=" flex justify-center h-3/5  ">
-                    <button  className="flex justify-center h-12 w-12 mx-2  hover:bg-zinc-300 rounded ">
-                        <FontAwesomeIcon  icon={faUtensils} size="2x"  className="text-green-800 flex justify-center mt-2"/>
-                    </button>
-                </div>
-                <div className=" flex justify-center h-2/5   " >
-                    <p className=" font-bold text-sm ">Restaurants</p>
-                </div>
-            </div>
-            <div className=" ">
-
-                <div className=" flex justify-center h-3/5  ">
-                    <button  className="flex justify-center h-12 w-12 mx-2  hover:bg-zinc-300 rounded ">
-                        <FontAwesomeIcon  icon={faBicycle} size="2x"  className="text-green-800 flex justify-center mt-2"/>
-                    </button>
-                </div>
-                <div className=" flex justify-center h-2/5   " >
-                    <p className=" font-bold text-sm ">Bike parking</p>
-                </div>
-            </div>
-            <div className=" ">
-
-                <div className=" flex justify-center h-3/5  ">
-                    <button  className="flex justify-center h-12 w-12 mx-2  hover:bg-zinc-300 rounded ">
-                        <FontAwesomeIcon  icon={faBuildingColumns} size="2x"  className="text-green-800 flex justify-center mt-2"/>
-                    </button>
-                </div>
-                <div className=" flex justify-center h-2/5  " >
-                    <p className=" font-bold text-sm ">Museums/culture</p>
-                </div>
-            </div>
         </>
 
     )
--- src/frontend/components/navigation/Sidebar.tsx
+++ src/frontend/components/navigation/Sidebar.tsx
@@ -1,56 +1,50 @@
 import React, {useState} from 'react';
 import Categories from "./Categories";
+
+
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBicycle, faBuildingColumns, faSquareParking, faToilet, faUtensils} from "@fortawesome/free-solid-svg-icons";
+import {faBicycle, faBuildingColumns, faSquareParking, faRestroom, faUtensils,
+    faCartShopping,faAnglesDown,faAnglesUp} from "@fortawesome/free-solid-svg-icons";
 
 export default function Sidebar (){
     const [isOpen, setIsOpen] = useState(false);
+
+    const [categories, setCategories] = useState([
+        { iconeName:faCartShopping, nameOfCategory:'Grocery' },
+        { iconeName:faSquareParking, nameOfCategory:'Car parking' },
+        { iconeName:faRestroom, nameOfCategory:'Restroom' },
+        { iconeName:faUtensils, nameOfCategory:'Restaurants' },
+        { iconeName:faBicycle, nameOfCategory:'Bike parking' },
+        { iconeName:faBuildingColumns, nameOfCategory:'Museums/Culture' }
+    ])
+
 
     return(
         <>
 
             {!isOpen?(
-
-
-                <div className=" flex justify-center z-10 bg-transparent">
+                <div className=" flex justify-center ">
                     <button onClick={()=>setIsOpen(!isOpen)}
                             className=" flex justify-center bg-green-500   h-5 w-12  hover:bg-cyan-500 text-white font-bold  rounded-b-full"
-                    > <svg  className="relative w-5 h-5 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" d="M15.707 4.293a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L10 8.586l4.293-4.293a1 1 0 011.414 0zm0 6a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L10 14.586l4.293-4.293a1 1 0 011.414 0z" clipRule="evenodd" /></svg>
+                    >  <FontAwesomeIcon  icon={faAnglesDown} size="sm"  className=" text-white flex justify-center mt-1 "/>
                     </button>
-
 
                 </div>
             ):(<>
-
                     <div className={` p-4 h-auto  bg-zinc-100 grid grid grid-cols-3 gap-6 p-4 z-10 rounded-b-2xl ${isOpen? 'translate-y-0':'translate-y-full'} ease-in-out duration-400 `} >
-
-                        <Categories/>
+                        <Categories categories={categories} />
                     </div>
-
-
-
-                    <div className="flex w-full flex-col   bg-transparent ">
 
                         <div className=" flex justify-center ">
                             <button onClick={()=>setIsOpen(!isOpen)}
                                     className=" flex justify-center bg-green-500   h-5 w-12  hover:bg-cyan-500 text-white font-bold  rounded-b-full"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" className="relative w-5 h-5 " fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 11l7-7 7 7M5 19l7-7 7 7"/>
-                                </svg></button>
+                            ><FontAwesomeIcon  icon={faAnglesUp} size="sm"  className=" text-white flex justify-center mt-1 "/>
+
+                            </button>
                         </div>
-                    </div>
-
-
 
                 </>
-            )
-
-            }
-
-
+            ) }
 
         </>
     )
