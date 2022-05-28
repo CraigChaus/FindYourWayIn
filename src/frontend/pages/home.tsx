5967663507b378e5a3170ad1c974a4f2f67a3683
Minh Pham <491537@student.saxion.nl>
2022-05-28 14:49:59 +0200

    fix: change page structure

--- src/frontend/pages/home.tsx
+++ src/frontend/pages/home.tsx
@@ -1,103 +1,18 @@
-import UserLocationMarker from "@components/GoogleMaps/userLocationMarker";
-import LocationMarker from "@components/LocationMarker";
-import React from "react";
-import GoogleAutocomplete from "../components/GoogleMaps/GoogleAutocomplete";
-import GoogleMap from "../components/GoogleMaps/GoogleMap";
+import type { NextPage } from 'next'
 
-const HomePage = () => {
-    const [ mounted, setMounted ] = React.useState(false);
-    // Default value set to Deventer in the case that geolocation doesnt work
-    const [ lat, setLat ] = React.useState(52.2661);
-    const [ lng, setLng ] = React.useState(6.1552);
-    const [ zoom, setZoom ] = React.useState(12);   
+import Navbar from "../components/navigation/Navbar";
+import HomePage from "../components/HomePage";
 
-    // Reverse geocode marker position
-    const geocoder = new google.maps.Geocoder;
-    const [ country, setCountry ] = React.useState< string >();
-    const [ city, setCity ] = React.useState< string >();
-    const [ sector, setSector ] = React.useState< string >();
-    const [ neighborhood, setNeighborhood ] = React.useState< string >();
-    const [ address, setAddress ] = React.useState< string >('');
 
-    const [isLocation, setIsLocation] = React.useState(false);
+const Home: NextPage = () => {
 
-    function handleSetLocation() {
-        setIsLocation(!isLocation);
-    }
-
-        React.useEffect(() => {
-            if(navigator.geolocation){
-                navigator.geolocation.getCurrentPosition(
-                    (pos) => {
-                        const position = pos.coords;
-                        if(position){
-                            setLat(position.latitude);
-                            setLng(position.longitude);
-                        }
-                    }
-                );
-            };
-            setMounted(true);
-        }, [isLocation]);
-
-    React.useEffect(() => {
-        if(!mounted) return;
-        geocoder
-            .geocode({ location: {lat, lng}})
-            .then(res => {
-                if(res.results[0]){
-                    res.results[0].address_components.reverse().filter((object) => {
-                        object.types.filter((type) => {
-                            if(type === 'country') setCountry(object.long_name);
-                            if(type === 'locality') setCity(object.long_name);
-                            if(type === 'sublocality_level_1') setSector(object.long_name);
-                            if(type === 'route') setAddress(s=>s+object.long_name);
-                            if(type === 'street_number') setAddress(s=>s+' '+object.long_name);
-                        });
-                    })
-                }
-                res.results.map(object => {
-                    object.types.filter(type => {
-                        if(type === 'neighborhood') setNeighborhood(object.formatted_address.split(',')[0]);
-                    });
-                });
-            });
-    }, [lat]);
-
-    console.log(`
-        Country: ${country}\n
-        City: ${city}\n
-        Address: ${address}
-    `);
-
-    return(
-        <div>
-            <GoogleAutocomplete
-                setLat={setLat}
-                setLng={setLng}
-                setAddress={setAddress}
-            />
-            
-            <GoogleMap 
-                center={{lat, lng}}
-                zoom={zoom}
-                setZoom={setZoom}
-                style={{width: '100%', height: '500px'}}
-                disableDefaultUI
-                clickableIcons={false}
-                mapId="9c7cb3e171b411ff"
-            >
-                <UserLocationMarker
-                    position={{lat, lng}}
-                    draggable
-                    setLat={setLat}
-                    setLng={setLng}
-                    setAddress={setAddress}
-                />
-            </GoogleMap>
-
-            <LocationMarker isLocation={isLocation} setIsLocation={handleSetLocation} />
+    return (
+        <div className="flex flex-col h-screen ">
+            <Navbar/>
+            <HomePage/>
         </div>
-    );
-};
-export default HomePage;
+    )
+}
+
+export default Home
+
