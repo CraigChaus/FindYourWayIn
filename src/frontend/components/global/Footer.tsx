1fae0b564e4e4549d2fe03f24781312c0c17112e
Minh Pham <491537@student.saxion.nl>
2022-06-01 11:56:01 +0200

    fix: link in Footer

--- src/frontend/components/global/Footer.tsx
+++ src/frontend/components/global/Footer.tsx
@@ -4,10 +4,11 @@ import Link from 'next/link';
 export default function Footer() {
     return (
         <footer className="absolute bottom-0 left-0 right-0 p-4 text-center text-white">
-            <span className="block text-sm text-gray-300 sm:text-center dark:text-gray-300">© 2022 
+            <span className="block text-sm text-gray-300 sm:text-center dark:text-gray-300">© 2022
                 <Link href="/home">
-                    <a className="hover:underline">FYWI™</a>. All rights reserved.
+                    <a className="hover:underline"> FYWI™</a>
                 </Link>
+                . All rights reserved.
             </span>
         </footer>
     )
