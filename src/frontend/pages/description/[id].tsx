2bebdc04713f6dd44587e088b8672e4731ef3990
Minh Pham <491537@student.saxion.nl>
2022-06-03 02:30:04 +0200

    feat: dynamically render details page based on location id

--- src/frontend/pages/description/[id].tsx
+++ src/frontend/pages/description/[id].tsx
@@ -4,17 +4,48 @@ import Schedule from "../../components/location-details/Schedule";
 import ContactDetails from "../../components/location-details/ContactDetails";
 import React from "react";
 import Layout from "@components/global/Layout";
-import LocationImages from "@components/location-details/LocationImages";
-import { NextRouter, useRouter } from 'next/router';
+
+export async function getStaticPaths() {
+  const res = await fetch('https://app.thefeedfactory.nl/api/locations', {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 0eebe5c7-cf95-4519-899b-59e1a78768c1'
+      },
+    });
+  const data = await res.json();
+  const dataArray = data.results
+  const paths = dataArray.map((location: any) => {
+    return {
+      params: { id: location.id }
+    }
+  });
+
+  return {
+    paths, fallback: false
+  };
+}
 
 
+export async function getStaticProps(context: { params: { id: string } }) {
+  const id = context.params.id;
+  const res = await fetch(`https://app.thefeedfactory.nl/api/locations/${id}`, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 0eebe5c7-cf95-4519-899b-59e1a78768c1'
+      },
+    });  
+  const data = await res.json();
 
-export const Details = (props: any): JSX.Element => {
-  let url = '';
-  const router: NextRouter = useRouter();
-  const { id } = router.query;
+  return {
+    props: {
+      data: data
+    }
+  };
+}
 
-  
+export const Details = ({ data }: any): JSX.Element => {
 
   const [locationName, setLocationName] = React.useState(null);
   const [description, setDescription] = React.useState(null);
@@ -24,50 +55,30 @@ export const Details = (props: any): JSX.Element => {
   const [phoneNumber, setPhoneNumber] = React.useState(null);
   const [email, setEmail] = React.useState(null);
   
-  //the url aand the id are passd in here for rendering
   React.useEffect(() => {
-    url = `https://app.thefeedfactory.nl/api/locations/6280fef98b779766af33c4e6`
-  }, [id]);
-  
-  React.useEffect(() => {
-    console.log(url);
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer 0eebe5c7-cf95-4519-899b-59e1a78768c1'
-        },
-      })
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
-          console.log(data);
+    if (data.calendar.patternDates[0]) {
+      setCalendar(data.calendar.patternDates[0].opens);
+    }
 
-          //api data for time table(schedule)
-          setCalendar(data.calendar.patternDates[0].opens);
+    //api data for name of the location
+    setLocationName(data.location.label);
 
-          //api data for name of the location
-          setLocationName(data.location.label);
+    //api data for the description of the place
+    setDescription(data.trcItemDetails[0].longdescription);
 
-          //api data for the description of the place
-          setDescription(data.trcItemDetails[0].longdescription);
-
-          //image of the place is caught here
-          setImgSrc(data.files[0].hlink);
-          setImgAlt('alt');
-
-          //phone number of the place is put here
-          setPhoneNumber(data.contactinfo.phone.number);
-
-          //email address for the place is put here
-          setEmail(data.contactinfo.mail.email);
-        })
+    //image of the place is caught here
+    if (data.files[0]) {      
+      setImgSrc(data.files[0].hlink);
+    }
     
-        .catch(error => {
-          throw new Error(`HTTP error! status: ${error.status}`);
-      })
-  }, []);
+    setImgAlt('alt');
+
+    //phone number of the place is put here
+    setPhoneNumber(data.contactinfo.phone.number);
+
+    //email address for the place is put here
+    setEmail(data.contactinfo.mail.email);
+  }, [data]);
 
   return (
     <Layout>
