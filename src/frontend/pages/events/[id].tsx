156f8f0fcc825a7549d4ee579f429f6a2a806281
CraigChaus <craigchauraya@gmail.com>
2022-06-04 14:12:48 +0200

    fix: Fix env file by renaming it

--- src/frontend/pages/events/[id].tsx
+++ src/frontend/pages/events/[id].tsx
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import DayInfo from '@components/eventsDetails/DayInfo';
 import EventImage from '@components/eventsDetails/EventImage';
 import EventInfo from '@components/eventsDetails/EventInfo';
-=======
-import AgendaInfo from '@components/events/AgendaInfo';
-import { UpcomingInfo } from '@components/events/UpcomingInfo';
-import { deleteApp } from 'firebase/app';
-import React from 'react';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 const apiKey = process.env.NEXT_PUBLIC_FEEDFACTORY_API_KEY;
->>>>>>> 2a7d0ff586acbf792f084dde8ff425b34dc0f8fb
 
 export async function getStaticPaths() {
     const res = await fetch(`${apiUrl}/events`, {
@@ -51,7 +45,6 @@ export async function getStaticProps(context: { params: { id: string } }) {
             data: data,
         },
     };
-<<<<<<< HEAD
   }
 
   export const Events = ({ data }: any): JSX.Element => {
@@ -66,12 +59,7 @@ export async function getStaticProps(context: { params: { id: string } }) {
     const [dayOfEvent, setDayOfEvent] = React.useState(null);
 
     //This is for the event name, time, venue, ticket info and website
-=======
-}
 
-export const Agenda = ({ data }: any): JSX.Element => {
-    //creating an instance of the event name and day as usestates (FOR CURRENT EVENTS)
->>>>>>> 2a7d0ff586acbf792f084dde8ff425b34dc0f8fb
     const [eventName, setEventName] = React.useState(null);
     const [eventStartTime, setEventStartTime] = React.useState(null);
     const [eventendTime, setEventEndTime] = React.useState(null);
@@ -79,7 +67,6 @@ export const Agenda = ({ data }: any): JSX.Element => {
     //this is for the event description
     const [eventDescription, setEventDescription] = React.useState(null);
 
-<<<<<<< HEAD
     //this is for the venue
     const [eventStreetVenue, setEventStreetVenue] = React.useState(null);
     const [eventHouseNumVenue, setEventHouseNumVenue] = React.useState(null);
@@ -155,60 +142,3 @@ export const Agenda = ({ data }: any): JSX.Element => {
   )
 }
 export default Events;
-=======
-    React.useEffect(() => {
-        //api data for the date of the event
-
-        //this is the instance of the devices current date and time
-        const today = new Date();
-
-        //here the date string is split into the day number and the month number
-        const todayDateNumber = today.getDate();
-        const thisMonthNumber = today.getMonth();
-
-        //this is the api data for the date of the venue
-        const dayNumber = data.calendar.singleDates[0].date;
-
-        //here the date string is split into the day number and the month number
-        const dayNumberInstance = dayNumber.substring(8, 10);
-        const monthNumberInstance = dayNumber.substring(5, 7);
-
-        //eliminating all leading zeros
-        const monthNumberNoZero = parseInt(monthNumberInstance, 10);
-
-        //only show the event date if the day is equal to the devices date and time or ahead of it
-        if (
-            dayNumberInstance >= todayDateNumber &&
-            thisMonthNumber == monthNumberNoZero
-        ) {
-            //api data for name of the event
-            setEventName(data.location.label);
-
-            //The date is being splitted to only show the date number of the event since this will be in the same month
-            setDay(dayNumberInstance);
-        } else if (monthNumberNoZero > thisMonthNumber) {
-            //(YYYY/MM/DD)
-            const fullDayNumber = dayNumber.substring(0, 10);
-
-            //the name of the event that is upcoming
-            setUpEventName(data.location.label);
-
-            //the day, month and year of the upcoming event
-            setUpDay(fullDayNumber);
-        }
-    }, [data]);
-
-    return (
-        <>
-            <div>
-                <h1 className="p-4 font-bold text-center">Events & Agenda</h1>
-                {<AgendaInfo date={day} event={eventName} />}
-
-                {<UpcomingInfo upDate={upDay} upEvent={upEventName} />}
-            </div>
-        </>
-    );
-};
-
-export default Agenda;
->>>>>>> 2a7d0ff586acbf792f084dde8ff425b34dc0f8fb
