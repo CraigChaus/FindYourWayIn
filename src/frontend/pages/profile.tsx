e0415a4a2994f02e1b5c8335e0b828213999a7ed
486702D <75730085+486702D@users.noreply.github.com>
2022-06-08 00:53:48 +0200

    styles(frontend/pages/profile): fixed styling problems

--- src/frontend/pages/profile.tsx
+++ src/frontend/pages/profile.tsx
@@ -8,16 +8,23 @@ const Profile = (props: any) => {
     return (
     <div className=" h-screen bg-cover bg-landing-page absolute w-full  text-gray-900 bg-cover bg-no-repeat ">
 
-    <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
+    <div className="h-2/3 max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0 shadow-md">
 
-        <div id="profile" className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
+        <div id="profile" className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-80 mx-6 lg:mx-0">
 
 
             <div className="p-4 md:p-12 text-center lg:text-left">
                 
                 <div  className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center bg-[url('../public/images/profileCat.png')]" ></div>
+                <div className="flex items-center justify-between">
+                    <p className=" text-xl font-semibold text-slate-900 text-gray-400 text-sm">Username: </p>
 
-                <h2 className="text-3xl font-bold pt-8 lg:pt-0">{user.email}</h2>
+                </div>
+                <div className="  w-full  bg-white rounded shadow-lg ml-3 ">
+                    <p className="text-2xl p-3 font-bold  lg:pt-0">{user.email}</p>
+                </div>
+
+
 
                 <div className="pt-12 pb-8">
                     <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
@@ -31,8 +38,8 @@ const Profile = (props: any) => {
         </div>
 
       
-        <div className="w-full lg:w-2/5">
-            <img src="./public/images/profileCat.png" className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block"/>
+        <div className="w-full   h-full lg:w-2/5">
+            <div  className="  bg-cover bg-no-repeat h-1/2 mt-40 hidden  rounded-none lg:rounded-lg shadow-2xl lg:block bg-[url('../public/images/profileCat.png')]"></div>
             
         </div>
 
