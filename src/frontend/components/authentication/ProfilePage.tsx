16813705c11130687c4137764ecd72adb5183d84
486702D <75730085+486702D@users.noreply.github.com>
2022-06-08 09:38:47 +0200

    navBar in profile page(frontend/component/ProfilePage): added NavBar to profile page

--- src/frontend/components/authentication/ProfilePage.tsx
+++ src/frontend/components/authentication/ProfilePage.tsx
@@ -1,13 +1,33 @@
-
+import SideBar from '../global/Sidebar';
+import UserDropdown from '../global/user/Dropdown';
 
 const ProfilePage = (props: any) => {
      const user = props.user;
 
     const categories = props.categories;
     return (
+     // this block I took from  component MapNavbar,
         <div className=" h-screen bg-cover bg-landing-page absolute w-full  text-gray-900 bg-cover bg-no-repeat ">
 
-            <div className="h-2/3 max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0 shadow-md">
+
+            <div className="z-10 flex flex-col w-full bg-transparent">
+                <div
+                    id="header"
+                    className="z-10 flex items-center justify-between bg-green-500 h-18"
+                >
+                    <SideBar />
+                    <div className="flex justify-end" >
+
+                        <UserDropdown />
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+            <div className="h-2/3 max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto mt-28 lg:my-0 shadow-md">
 
                 <div id="profile" className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-80 mx-6 lg:mx-0">
 
@@ -23,8 +43,7 @@ const ProfilePage = (props: any) => {
                             <p className="text-2xl p-5 font-bold  lg:pt-0">{user.email}</p>
                         </div>
 
-
-
+                        
                         <div className="pt-12 pb-8">
                             <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
                                 My preferences
@@ -35,16 +54,12 @@ const ProfilePage = (props: any) => {
                     </div>
 
                 </div>
-
-
+                
                 <div className="w-full   h-full lg:w-2/5">
                     <div  className="  bg-cover bg-no-repeat h-1/2 mt-40 hidden  rounded-none lg:rounded-lg shadow-2xl lg:block bg-[url('../public/images/profileCat.png')]"></div>
 
                 </div>
 
-                <div className="absolute top-0 right-0 h-12 w-18 p-4">
-                    <button className="js-change-theme focus:outline-none">🌙</button>
-                </div>
 
             </div>
         </div>
