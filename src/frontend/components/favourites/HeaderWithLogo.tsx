748f0107cd26666ac0852817e817e101e0d46828
AndreiLidzhiev <480211@student.saxion.nl>
2022-06-09 23:43:04 +0200

    code: sample tabs added

---
+++ src/frontend/components/favourites/HeaderWithLogo.tsx
@@ -0,0 +1,15 @@
+const headerWithLogo = () => {
+
+
+
+    return <>
+        <div className="flex flex-col w-full bg-transparent">
+            <div
+                className="flex items-center justify-between bg-green-500 h-18"
+            >
+        </div>
+    </div>
+    </>
+}
+
+export default headerWithLogo
