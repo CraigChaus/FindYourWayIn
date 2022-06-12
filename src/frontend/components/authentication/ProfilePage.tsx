aabd858be014ca0e6f21c63e16bbf1f70b92f9a8
486702D <75730085+486702D@users.noreply.github.com>
2022-06-12 17:59:51 +0200

    fixing bags on profile page(frontend/components/ProfilePage): fixing problems with styles

--- src/frontend/components/authentication/ProfilePage.tsx
+++ src/frontend/components/authentication/ProfilePage.tsx
@@ -1,12 +1,13 @@
 import SideBar from '../global/Sidebar';
 import UserDropdown from '../global/user/Dropdown';
+import Footer from "@components/global/Footer";
 
 const ProfilePage = (props: any) => {
     const user = props.user;
 
     const categories = props.categories;
     return (
-        <div className=" h-screen bg-cover bg-landing-page absolute w-full bg-center bg-no-repeat ">
+        <div className=" h-screen bg-cover bg-landing-page absolute w-full bg-center  overflow-y-scroll">
             {/*// this block I took from  component MapNavbar,*/}
             <div className="z-10 flex flex-col w-full bg-transparent">
                 <div
@@ -27,19 +28,27 @@ const ProfilePage = (props: any) => {
                     className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-80 mx-6 lg:mx-0"
                 >
                     <div className="p-4 md:p-12 text-center lg:text-left">
-                        <div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center bg-[url('../public/images/profileCat.png')]"></div>
-                        <div className="flex items-center justify-between">
-                            <p className=" text-2xl font-semibold  text-gray-400 ">
+                        <div className="block lg:hidden   rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center bg-[url('../public/images/profileCat.png')]"></div>
+
+                        <div className="flex justify-center  border-b-2 mb-3 mt-3 mx-4 pb-2">
+                            <p className=" text-2xl font-bold  text-black ">
+                                My profile:
+                            </p>
+                        </div>
+
+
+                        <div className="flex justify-start   mb-1">
+                            <p className=" text-md   text-gray-500 ">
                                 Username:{' '}
                             </p>
                         </div>
-                        <div className="  w-full  bg-white rounded shadow-lg ml-3 ">
-                            <p className="text-2xl p-5 font-bold  lg:pt-0">
+                        <div className="  flex justify-start  bg-white rounded shadow-lg  ">
+                            <p className="text-md  font-bold  ">
                                 {user.email}
                             </p>
                         </div>
 
-                        <div className="pt-12 pb-8">
+                        <div className="pt-6 pb-6">
                             <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
                                 My preferences
                             </button>
@@ -48,9 +57,10 @@ const ProfilePage = (props: any) => {
                 </div>
 
                 <div className="w-full   h-full lg:w-2/5">
-                    <div className="  bg-cover bg-no-repeat h-1/2 mt-40 hidden  rounded-none lg:rounded-lg shadow-2xl lg:block bg-[url('../public/images/profileCat.png')]"></div>
+                    <div className="  bg-cover bg-no-repeat h-80 mt-28 hidden  rounded-none lg:rounded-lg shadow-2xl lg:block bg-[url('../public/images/profileCat.png')]"></div>
                 </div>
             </div>
+            <Footer/>
         </div>
     );
 };
