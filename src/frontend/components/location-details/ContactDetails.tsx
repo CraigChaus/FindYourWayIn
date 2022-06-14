f124218de3294bb411f592882675074014900e30
vedatdd <90068068+vedatdd@users.noreply.github.com>
2022-06-14 14:38:49 +0200

    chore:fixed prettier

--- src/frontend/components/location-details/ContactDetails.tsx
+++ src/frontend/components/location-details/ContactDetails.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { useAuth } from 'contexts/AuthContext';
 
-
 export const ContactDetails = ({ phoneNumber, email }: any) => {
     const { user } = useAuth();
-    const addToFavorites = () =>{
+    const addToFavorites = () => {
         // TODO: write the actual firebase command to add location id
-    }
+    };
 
     return (
         <div className="pl-2 text-left w-full">
             <h2 className="font-bold text-center underline underline-offset-8">
                 Contact details
-            </h2>   
+            </h2>
             <p>{phoneNumber} ---------</p>
             <p>{email} ---------</p>
-            <button className="border-2 border-blue-800" onClick={()=>console.log(`user logging:${user.uid}`)}>
+            <button
+                className="border-2 border-blue-800"
+                onClick={() => console.log(`user logging:${user.uid}`)}
+            >
                 Add to favourites
             </button>
         </div>
