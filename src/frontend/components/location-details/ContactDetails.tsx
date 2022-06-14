2770cdb5c88b9c02a29c90df844bb8f4c64ff334
vedatdd <90068068+vedatdd@users.noreply.github.com>
2022-06-14 15:19:06 +0200

    feat: add location id to db on click

--- src/frontend/components/location-details/ContactDetails.tsx
+++ src/frontend/components/location-details/ContactDetails.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useAuth } from 'contexts/AuthContext';
+import { db } from 'firebase_config';   
+import { doc, setDoc,updateDoc, arrayUnion, arrayRemove  } from "firebase/firestore"; 
+
 
 export const ContactDetails = ({ phoneNumber, email, id }: any) => {
     const { user } = useAuth();
-    const addToFavorites = () => {
+    const addToFavorites = async () => {
         // TODO: write the actual firebase command to add location id
-    };
+        const userRef = doc(db, 'users', user.uid);
+        await updateDoc(userRef, {
+            favorite_locations:arrayUnion(id)
+        });
+
+        
+        console.log("new location added to the users favorites!")
+   }
 
     return (
         <div className="pl-2 text-left w-full">
@@ -16,12 +26,13 @@ export const ContactDetails = ({ phoneNumber, email, id }: any) => {
             <p>{email} ---------</p>
             <button
                 className="border-2 border-blue-800"
-                onClick={() => console.log("user id", user.uid, "location id", id)}
+                onClick={addToFavorites}
             >
                 Add to favourites
             </button>
         </div>
     );
 };
+
 
 export default ContactDetails;
