ee8b294edfb3fcbd3f544a74521797438e30d6b0
486702D <75730085+486702D@users.noreply.github.com>
2022-06-16 00:47:40 +0200

    searching alg(frontend/ components/map-navbar/LocationComponent and SearchInputForm): added styles ,function collapse on leave the input, added address and zipcode for component with locations

--- src/frontend/components/map-navbar/LocationComponent.tsx
+++ src/frontend/components/map-navbar/LocationComponent.tsx
@@ -1,15 +1,16 @@
 
 const LocationComponent= (props: any) => {
     const locationRes = props.locationRes;
-    const title=props.title;
+    //const title=props.title;
 
 
 
 
     return(<>
         {locationRes.map((location: any, index: number) => (
-        <div key={index + 1} className="  flex-col w-full border-2">
-            <div className="  w-full h-10 text-black"><p>{location.title}</p></div>
+        <div key={index + 1} className="  flex-col w-full border-1 pl-1">
+            <div className="  w-full h-6 text-green-900"><p> {location.title}</p></div>
+            <div className="  w-full h-5 text-black text-sm"><p>  {location.street} {location.houseNumber}, {location.city} {location.country}. {location.zipcode}</p></div>
             </div>
             ))}
     </> );
