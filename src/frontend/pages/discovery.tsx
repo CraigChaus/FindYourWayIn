868c4476d94794b61ad33de2536c7100ca47b570
CraigChaus <craigchauraya@gmail.com>
2022-06-16 01:11:32 +0200

    feat: Connect to API so as to fetch data

--- src/frontend/pages/discovery.tsx
+++ src/frontend/pages/discovery.tsx
@@ -1,10 +1,10 @@
-import { Data } from '@react-google-maps/api';
 import React from 'react';
 import DiscoveryComponent from '../components/discovery/DiscoveryComponent';
 
 type DiscoveryProp = {
     id:any;
     imageSRC: any;
+    imageAlt: any;
     locationName: any;
 };
 
@@ -29,43 +29,52 @@ export async function getStaticProps() {
 }
 
 export const Discovery = ({data}:any): JSX.Element => {
-    // const router = useRouter();
-    console.log(data);
 
     const [locationData, setLocationData] = React.useState< DiscoveryProp[] | null>(null);
 
     const [spotLightId,setSpotLightId] = React.useState(null);
     const [spotLightName, setSpotLightName] = React.useState(null);
     const [spotLightImage, setSpotLightImage] = React.useState(null);
+    const [spotLightImageAlt,setSpotLightImageAlt] = React.useState<any>(null);
 
     React.useEffect(() => {
+
         const resultLocation = [];
 
         const total = (data.results.length);
+        console.log(total)
 
         for (let i = 0; i < data.results.length; i++) {
-            resultLocation.push({
-                id: data.results[i].id,
-                imageSRC: data.results[i].files[0]?.hlink,
-                locationName: data.results[i].location?.label,
-            });
 
+                resultLocation.push({
+                    id: data.results[i]?.id,
+                    imageSRC: data.results[i]?.files[0]?.hlink,
+                    imageAlt: 'alt',
+                    locationName: data.results[i]?.location?.label,
+            });
         }
 
+        //This is the random index number generator based on total number of array elements
         let spotLightPicker = Math.floor(Math.random() * 10) % (total ?? 0) + (total ? 0 : 0);
-        console.log(spotLightPicker);
 
-        setSpotLightName(data.results[spotLightPicker].location?.label)
-        setSpotLightImage(data.results[spotLightPicker].files[0]?.hlink)
-        setSpotLightId(data.results[spotLightPicker].id)
+        setSpotLightName(data.results[spotLightPicker].location?.label);
+
+        setSpotLightImage(data.results[spotLightPicker]?.files[0]?.hlink);
+        
+        setSpotLightId(data.results[spotLightPicker].id);
+        setSpotLightImageAlt('alt');
                
         setLocationData(resultLocation);
     },[data]);
 
-    return <DiscoveryComponent locationData={locationData} 
-                               spotLightName = {spotLightName}
-                               spotLightImage = {spotLightImage}
-                               spotLightID = {spotLightId} />;
+    return (
+    <DiscoveryComponent locationData={locationData} 
+                               spotName = {spotLightName}
+                               spotImage = {spotLightImage}
+                               spotID = {spotLightId}
+                               spotImageAlt = {spotLightImageAlt} />
+    
+    );
 };
 
 export default Discovery;
