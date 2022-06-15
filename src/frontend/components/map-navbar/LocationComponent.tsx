c3064b545319118a8e70a7c8eb95120b3d6fd618
486702D <75730085+486702D@users.noreply.github.com>
2022-06-16 01:32:20 +0200

    SearchInputForm(frontend/component/map-navbar/LocationComponent and SearchInputComponent): after "npm run lint-fix"

--- src/frontend/components/map-navbar/LocationComponent.tsx
+++ src/frontend/components/map-navbar/LocationComponent.tsx
@@ -1,15 +1,27 @@
-
-const LocationComponent= (props: any) => {
+const LocationComponent = (props: any) => {
     const locationRes = props.locationRes;
 
-    return(<>
-        {locationRes.map((location: any, index: number) => (
-        <div key={index + 1} className="  flex-col w-full rounded-b-lg  border-1 pl-1">
-            <div className="  w-full h-6 text-green-900"><p> {location.title}</p></div>
-            <div className="  w-full h-5 text-black text-sm"><p>  {location.street} {location.houseNumber}, {location.city} {location.country}. {location.zipcode}</p></div>
-            </div>
+    return (
+        <>
+            {locationRes.map((location: any, index: number) => (
+                <div
+                    key={index + 1}
+                    className="  flex-col w-full rounded-b-lg  border-1 pl-1"
+                >
+                    <div className="  w-full h-6 text-green-900">
+                        <p> {location.title}</p>
+                    </div>
+                    <div className="  w-full h-5 text-black text-sm">
+                        <p>
+                            {' '}
+                            {location.street} {location.houseNumber},{' '}
+                            {location.city} {location.country}.{' '}
+                            {location.zipcode}
+                        </p>
+                    </div>
+                </div>
             ))}
-    </> );
-
+        </>
+    );
 };
 export default LocationComponent;
