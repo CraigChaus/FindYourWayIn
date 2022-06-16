4580133227b6c9f6dc5cc2de819723b7d104be57
CraigChaus <craigchauraya@gmail.com>
2022-06-16 21:52:12 +0200

    fix: Add data checker for location name and image, replace null image with broken.png

--- src/frontend/pages/discovery.tsx
+++ src/frontend/pages/discovery.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DiscoveryComponent from '../components/discovery/DiscoveryComponent';
+import broken from '../public/images/broken.png'
 
 type DiscoveryProp = {
     id: any;
@@ -10,6 +11,10 @@ type DiscoveryProp = {
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 const apiKey = process.env.NEXT_PUBLIC_FEEDFACTORY_API_KEY;
+
+//these variables are for the location image and location name data checking stage
+let locImage:any = "";
+let locName:any = " ";
 
 export async function getStaticProps() {
     const res = await fetch(`${apiUrl}/locations`, {
@@ -34,8 +39,8 @@ export const Discovery = ({ data }: any): JSX.Element => {
     >(null);
 
     const [spotLightId, setSpotLightId] = React.useState(null);
-    const [spotLightName, setSpotLightName] = React.useState(null);
-    const [spotLightImage, setSpotLightImage] = React.useState(null);
+    const [spotLightName, setSpotLightName] = React.useState<any>(null);
+    const [spotLightImage, setSpotLightImage] = React.useState<any>(null);
     const [spotLightImageAlt, setSpotLightImageAlt] = React.useState<any>(null);
 
     React.useEffect(() => {
@@ -45,28 +50,53 @@ export const Discovery = ({ data }: any): JSX.Element => {
         console.log(total);
 
         for (let i = 0; i < data.results.length; i++) {
-            resultLocation.push({
-                id: data.results[i]?.id,
-                imageSRC: data.results[i]?.files[0]?.hlink,
-                imageAlt: 'alt',
-                locationName: data.results[i]?.location?.label,
-            });
+
+                //data checking stage
+                if((data.results[i]?.files[0]?.hlink)==null){
+                     locImage = broken;
+              
+                }else {
+
+                     locImage = data.results[i]?.files[0]?.hlink;
+                }
+                
+                if((data.results[i]?.trcItemDetails[0]?.title) == ""){
+                   
+                     locName = 'Under Construction';
+                }else{
+                    locName = data.results[i]?.trcItemDetails[0]?.title;
+                }
+
+                resultLocation.push({
+                    id: data.results[i]?.id,
+                    imageSRC: locImage,
+                    imageAlt: 'alt',
+                    locationName: locName
+                });
         }
 
         //This is the random index number generator based on total number of array elements
         const spotLightPicker =
             (Math.floor(Math.random() * 10) % (total ?? 0)) + (total ? 0 : 0);
 
-        setSpotLightName(data.results[spotLightPicker].location?.label);
+        if((data.results[spotLightPicker]?.trcItemDetails[0]?.title) == ""){
+            setSpotLightName('Under Construction');
+        }else{
+            setSpotLightName(data.results[spotLightPicker]?.trcItemDetails[0]?.title);
+        }
 
-        setSpotLightImage(data.results[spotLightPicker]?.files[0]?.hlink);
+        if((data.results[spotLightPicker]?.files[0]?.hlink) == null ){
+            setSpotLightImage(broken)
+        }else{
+          setSpotLightImage(data.results[spotLightPicker]?.files[0]?.hlink);
+        }
 
         setSpotLightId(data.results[spotLightPicker].id);
         setSpotLightImageAlt('alt');
 
         setLocationData(resultLocation);
     }, [data]);
-
+console.log(data);
     return (
         <DiscoveryComponent
             locationData={locationData}
