08a5a817fa9421a669087cfb55c074258cc8885a
Minh Pham <491537@student.saxion.nl>
2022-06-16 16:09:00 +0200

    fix: i18next configuration

--- src/frontend/next-i18next.config.js
+++ src/frontend/next-i18next.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'nl'],
-      localePath: path.resolve('./public/locales')
-    },
-  };
+import { resolve } from 'path';
+
+export const i18n = {
+  defaultLocale: 'en',
+  locales: ['en', 'nl'],
+  localePath: resolve('./public/locales')
+};
