53b1ce42ad11e3343e56bc77d52f6a7b51c71d78
CraigChaus <craigchauraya@gmail.com>
2022-06-17 20:41:03 +0200

    chore: Run lint and prettier fix

--- src/frontend/components/events/Card.tsx
+++ src/frontend/components/events/Card.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import broken from '../../public/images/broken.png'
+import broken from '../../public/images/broken.png';
 
 export const Card = ({ date, event, onClick, imageSrc }: any) => {
     return (
@@ -17,29 +17,31 @@ export const Card = ({ date, event, onClick, imageSrc }: any) => {
 
                     {/* change text color based on image color */}
 
-                    {imageSrc == broken ?
-                    <div className="flex flex-col relative  bottom-40">
-                        <span className="pr-2 text-center text-black font-bold text-l ">{date}</span>
-                        <span className="pl-2 text-center  text-black font-bold text-xl w-full ">
-                            {event}{' '}
-                        </span>
-                        <button className="mt-4 mx-24  bg-green-800 rounded-md text-white font-normal">
-                            Click for more
-                        </button>
-                    </div>
-                    :
-                    <div className="flex flex-col relative  bottom-40">
-                        <span className="pr-2 text-center text-white font-bold text-l ">{date}</span>
-                        <span className="pl-2 text-center  text-white font-bold text-xl w-full ">
-                            {event}{' '}
-                        </span>
-                        <button className="mt-4 mx-24  bg-green-800 rounded-md text-white font-normal">
-                            Click for more
-                        </button>
-                    </div>
-
-                    }
-
+                    {imageSrc == broken ? (
+                        <div className="flex flex-col relative  bottom-40">
+                            <span className="pr-2 text-center text-black font-bold text-l ">
+                                {date}
+                            </span>
+                            <span className="pl-2 text-center  text-black font-bold text-xl w-full ">
+                                {event}{' '}
+                            </span>
+                            <button className="mt-4 mx-24  bg-green-800 rounded-md text-white font-normal">
+                                Click for more
+                            </button>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col relative  bottom-40">
+                            <span className="pr-2 text-center text-white font-bold text-l ">
+                                {date}
+                            </span>
+                            <span className="pl-2 text-center  text-white font-bold text-xl w-full ">
+                                {event}{' '}
+                            </span>
+                            <button className="mt-4 mx-24  bg-green-800 rounded-md text-white font-normal">
+                                Click for more
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
         </>
