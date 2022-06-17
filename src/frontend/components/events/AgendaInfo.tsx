7245dab47c6029f796cc963306d9fa3fd7c7fac8
Craig Chauraya <500215@student.saxion.nl>
2022-06-17 17:01:25 +0200

    feat: Add card design to the event objects

--- src/frontend/components/events/AgendaInfo.tsx
+++ src/frontend/components/events/AgendaInfo.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import Image from 'next/image';
 
-export const AgendaInfo = ({ date, event, onClick }: any) => {
+export const AgendaInfo = ({ date, event, onClick , imageSrc }: any) => {
     return (
         <>
-            <div onClick={onClick} className="w-full h-1/2">
-                <div className="flex justify-between px-8 py-2 ml-8 mr-4 border-black border-t-2 border-b-2 border-solid mb-4">
-                    <span className="pr-2">{date}</span>
-                    <span className="pl-2 w-full">{event} </span>
+            <div onClick={onClick} className="w-full h-1/2" > 
+                <div className=" px-4 text-center h-60 m-1 p-2 border-1 border-black">
+                    <Image
+                    className='rounded-xl'
+                        src={imageSrc}
+                        alt={'alt'}
+                        height={270}
+                        width={400}
+                    />
+                    <div className='flex flex-col relative  bottom-40'>
+                        <span className="pr-2 text-center ">{date}</span>
+                        <span className="pl-2 text-center w-full ">{event} </span>
+                        <button className='mt-4 mx-24  bg-green-800'>Click for more</button>
+                    </div>
                 </div>
-            </div>
+           </div>
         </>
     );
 };
-
 export default AgendaInfo;
--- src/frontend/components/events/UpcomingInfo.tsx
+++ src/frontend/components/events/UpcomingInfo.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import Image from 'next/image';
 
-export const UpcomingInfo = ({ upDate, upEvent, onClick }: any) => {
+export const UpcomingInfo = ({ upDate, upEvent, onClick,upImageSrc }: any) => {
     return (
         <>
-            <div onClick={onClick}>
-                <div className="flex justify-between px-8 py-2 ml-8 mr-4 border-black border-t-2 border-b-2 border-solid">
-                    <span className="pr-2">{upDate}</span>
-                    <span className="pl-2 w-full">{upEvent}</span>
+            <div onClick={onClick} className="w-full h-1/2">
+                <div className="px-4 text-center h-60 m-1 p-2 border-1 border-black">
+                    <Image
+                            className='rounded-xl'
+                            src={upImageSrc}
+                            alt={'alt'}
+                            height={270}
+                            width={400}
+                        />
+                   <div className='flex flex-col relative  bottom-40'>
+                        <span className="pr-2 text-center">{upDate}</span>
+                        <span className="pl-2 text-center w-full ">{upEvent} </span>
+                        <button className='mt-4 mx-24 '>Click for more</button>
+                    </div>
                 </div>
             </div>
         </>
