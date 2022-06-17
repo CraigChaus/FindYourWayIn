6741535092ecf08a122cc26b50f80ccc9993bc0b
Minh Pham <491537@student.saxion.nl>
2022-06-17 21:48:52 +0200

    fix: prettier fix

--- src/frontend/cypress/integration/authentication_tests/auth_login.spec.js
+++ src/frontend/cypress/integration/authentication_tests/auth_login.spec.js
@@ -1,50 +1,42 @@
 /// <reference types="Cypress" />
-describe("Authenticaton: Logging in Valid Credentials-Good Weather", () =>{
-    it("type    input for email", () =>{
-        cy.visit('/auth/login')
+describe('Authenticaton: Logging in Valid Credentials-Good Weather', () => {
+    it('type    input for email', () => {
+        cy.visit('/auth/login');
         cy.get('input[name="auth-email"]')
-        .should("exist")
-        .type('test@gmail.com')
-    })
+            .should('exist')
+            .type('test@gmail.com');
+    });
 
-    it("type input for password",()=>{
-        cy.get('input[name="auth-password"]')
-        .should("exist")
-        .type("123456")
-    })
+    it('type input for password', () => {
+        cy.get('input[name="auth-password"]').should('exist').type('123456');
+    });
 
     it('click login button', () => {
-        cy.get('button').contains('Login')
-        .click()
-    })
+        cy.get('button').contains('Login').click();
+    });
 
-     it('check if auth was valid', () => {
-        cy.url().should('eq','http://localhost:3000/home')
-    })
-    
-})
+    it('check if auth was valid', () => {
+        cy.url().should('eq', 'http://localhost:3000/home');
+    });
+});
 
-describe("Authenticaton: Logging in Valid Credentials-Bad Weather", () =>{
-    it("type input for email", () =>{
-        cy.visit('http://localhost:3000/auth/login')
+describe('Authenticaton: Logging in Valid Credentials-Bad Weather', () => {
+    it('type input for email', () => {
+        cy.visit('http://localhost:3000/auth/login');
         cy.get('input[name="auth-email"]')
-        .should("exist")
-        .type('test@gmail.com')
-    })
+            .should('exist')
+            .type('test@gmail.com');
+    });
 
-    it("type input for password",()=>{
-        cy.get('input[name="auth-password"]')
-        .should("exist")
-        .type("123456")
-    })
+    it('type input for password', () => {
+        cy.get('input[name="auth-password"]').should('exist').type('123456');
+    });
 
     it('click login', () => {
-        cy.get('button').contains('Login')
-        .click()
-    })
+        cy.get('button').contains('Login').click();
+    });
 
-    
-     it('check if auth was valid', () => {
-        cy.url().should('eq','http://localhost:3000/auth/login')
-    })
-})
+    it('check if auth was valid', () => {
+        cy.url().should('eq', 'http://localhost:3000/auth/login');
+    });
+});
