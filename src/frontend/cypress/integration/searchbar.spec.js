4c55e6e29c5556d36260bae7a6e9adfbbb0ccb74
486702D <75730085+486702D@users.noreply.github.com>
2022-06-24 13:33:24 +0200

    tests for SearchBar(frontend/cypress/integration/searchbar):searchBar rest tests

--- src/frontend/cypress/integration/searchbar.spec.js
+++ src/frontend/cypress/integration/searchbar.spec.js
@@ -5,6 +5,11 @@ describe('type in to search bar', () => {
         cy.get('input[placeholder="Search a place"]').should('exist');
     });
 
+    it('focus on input field', () => {
+        cy.get('input[placeholder="Search a place"]').focus();
+    });
+
+
     it('type in search bar', () => {
         cy.get('input[placeholder="Search a place"]')
             .type('Speel')
@@ -15,9 +20,11 @@ describe('type in to search bar', () => {
         cy.get('div').contains('Speel je Wijs').should('exist');
     });
 
+
     it('dropdown menu is exist', () => {
         cy.get('#testSearch').should('exist');
     });
+
 
     it('Clicked on dropdown menu', () => {
         cy.get('#testSearch').click();
@@ -26,6 +33,43 @@ describe('type in to search bar', () => {
     it('bottom Slider appeared', () => {
         cy.get('#bottomSlider').should('exist');
     });
+
+    it('delete text from input', () => {
+        cy.get('input[placeholder="Search a place"]').clear();
+
+    });
+
+    it('check if it is empty', () => {
+        cy.get('input[placeholder="Search a place"]').should('be.empty');
+    });
+
+    it('check if after deleting we do not have dropdown element', () => {
+        cy.get('#slideSearchBar').should('not.exist');
+    });
+
+
+    it('type in search bar to check after if it is scrollable', () => {
+        cy.get('input[placeholder="Search a place"]')
+            .type('Deventer')
+            .type('{enter}')
+
+    });
+
+
+    it('scroll to bottom, to check function of scrollability', () => {
+        cy.get('#slideSearchBar').scrollTo("bottom").click()
+    });
+
+
+
+    // it('check if after deleting we do not have dropdown element', () => {
+    //     cy.get('#slideSearchBar').should('not.exist');
+    // });
+
+    // CypressTools.getByName(inputFieldName)
+    //     //     .should('be.visible')              // check the DOM element, passes it on as subject
+    //     //     .invoke('val')                     // changes subject to the text of the input
+    //     //     .should('not.be.empty');           // check the text is not empty
 
     // TODO: Click on the element
 
