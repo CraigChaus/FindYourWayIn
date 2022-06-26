41afdf992e34bc080b71181d524d03099bd6e865
486702D <75730085+486702D@users.noreply.github.com>
2022-06-26 18:08:47 +0200

    test for Categories menu(frontend/cypress/integration/categoryButtonsOnSlideBar): created test for slider menu with Categories

--- src/frontend/cypress/integration/mapNavBar.spec.js
+++ src/frontend/cypress/integration/mapNavBar.spec.js
@@ -1,13 +1,10 @@
 context('Map NavBar', () => {
 
-    it('Contact page is exist and there is Nav-bar', () => {
+    it('Home page is exist and there is Nav-bar', () => {
         cy.visit('/home');
-    });
-
-
-    it('should have a map-navbar', () => {
         cy.get('#mapNavBar').should('exist');
     });
+
 
     it('should have searchBar field', () =>{
         cy.get('input[placeholder="Search a place"]').should('exist');
